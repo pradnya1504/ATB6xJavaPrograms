341832753590754d1b843e177c8497263af619ef
pradnya1504 <165540609+pradnya1504@users.noreply.github.com>
2024-04-11 11:54:16 +0530

    Update Lab002.java

--- Lab002.java
+++ Lab002.java
@@ -1,6 +1,6 @@
 package ex_11042024;
 /**
- * Author - Pramod
+ * Author - Riya
  * Program Name - ATB6x
  *  Lab No - 2
  *  Concept - System , print ln
@@ -14,14 +14,14 @@ package ex_11042024;
 
 public class Lab002 {
     public static void main(String[] args) {
-        System.out.print("Hello Pradnya");
+        System.out.print("Hello Riya");
         System.out.print("Hello Shetty\t");
 
         // print -> in the end it will not add  \n - newline
         // println -> add a new line
 
-        System.out.println("Shrinath");
-        System.out.println("Shetty");
+        System.out.println("Ayesha");
+        System.out.println("Rajput");
     }
 
 
