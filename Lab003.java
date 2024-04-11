d43e4568de5759505d54313c6f21a7d2ddb783ba
pradnya1504 <165540609+pradnya1504@users.noreply.github.com>
2024-04-11 11:55:25 +0530

    Update Lab003.java

--- Lab003.java
+++ Lab003.java
@@ -8,7 +8,7 @@ package ex_11042024;
 //code to show that after String[] anything can be added in place of args
 
 public class Lab003 {
-    public static void main(String[] pradnya) {
+    public static void main(String[] priya) {
         System.out.println("1");
         System.out.println("2");
         System.out.println("3");
